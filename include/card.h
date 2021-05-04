f698d059cd6fe279e8efe5d4da6d3032a1269d2c
Cyphantasm <82920641+Cyphantasm@users.noreply.github.com>
2021-05-04 15:00:56 +0800

    Update card.h
    Adjust some definitions and function interfaces to fit player operations

--- include/card.h
+++ include/card.h
@@ -1,30 +1,37 @@
 #include <iostream>
-#include <queue>
+#include <vector>
 using namespace std;
 
-enum COLOR{RED_PEACH, BLACK_PEACH, RED_RECT, BLACK_FLOWER, BIG_KING, SMALL_KING};
+enum COLOR{SPADES, HEARTS, DIAMONDS, CLUBS, BIG_JOKER, SMALL_JOKER};
+enum RANK{ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, JOKER};
+
 class card
 {
 private:
-    int point;
-    COLOR color;
+    int point;//point get by the player if fetched
+    COLOR color;//pattern of the card
+    RANK rank;
 public:
-    card();
-    card(int newPoint, COLOR newColor): point(newPoint), color(newColor){};
-    ~card();
+    card(){};
+    card(int newPoint, COLOR newColor, RANK newRank): point(newPoint), color(newColor), rank(newRank) {};
+    ~card(){};
     int getPoint() const;
     COLOR getColor() const;
+    RANK getRank() const;
 };
 
 
 class cardHeap
 {
 private:
-    queue<card> cards;
+    vector<card> cards;//the container of cards
 public:
-    cardHeap();
-    ~cardHeap();
+    cardHeap() {};
+    ~cardHeap() {};
     bool isEmpty();
-    void initRandom();//initialize a new heap (already constructed) with random order cards
-    card& fetchCard();//get a card from the top of the heap
+    void initRandom();
+    card& fetchCard();
+    void insertCard(card& cardToInsert);
+    card& peekCard(int index);
 };
+
