ebbdac59b6e668a3438e945553f5f2533af504f0
timmywanttolearn <466904389@qq.com>
2021-05-04 21:43:33 +0800

    player.cpp施工完毕

--- cardgame/player_web/player.cpp
+++ cardgame/player_web/player.cpp
@@ -1,8 +1,75 @@
 #include<iostream>
 #include"player.h"
 #include"card.h"
+/**
+ * @brief create a player
+ * 
+ */
 player::player():score(0),playerHeap(),win(0),round(0){};
+
+
+
+
+/**
+ * @brief return this player's score
+ * 
+ * @return score
+ */
+int player::get_score()
+{
+    return this->score;
+}
+
+/**
+ * @brief get a card for a player
+ * 
+ * @param card_fetched
+ */
+void player::player_fetchCard(card card_fetched)
+{
+    this->playerHeap.insertCard(card_fetched);
+    return;
+}
+/**
+ * @brief set win to 0 or 1
+ * 
+ * @param a 
+ */
+void player::set_win(bool a)
+{
+    this->win=a;
+    return;
+}
+/**
+ * @brief set round to 0 or 1
+ * 
+ * @param a 
+ */
+void player::set_round(bool a)
+{
+    this->round=a;
+    return;
+}
+
+/**
+ * @brief set score to a value
+ * 
+ * @param a 
+ */
+void player::set_score(int a)
+{
+    this->score=a;
+    return;
+}
+/**
+ * @brief init a player, used when every round is over
+ * 
+ */
 void player::init_player()
 {
-    this->playerHeap=
+    this->set_score(0);
+    this->playerHeap.removeAllCards();
+    this->set_win(0);
+    this->set_round(0);
+    return;
 }
